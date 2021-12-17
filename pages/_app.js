b74bb06c84643d422472c93f2710356ceb81b352
Jonathan Yngfors <35510079+vrease@users.noreply.github.com>
2021-12-17 21:27:54 +0100

    Document App

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,14 @@ import '@assets/main.css'
 import '@assets/chrome-bug.css'
 import 'keen-slider/keen-slider.min.css'
 
-import { FC, useEffect } from 'react'
-import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
 
-const Noop: FC = ({ children }) => <>{children}</>
+const Noop = ({ children }) => <>{children}</>
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const Layout = (Component as any).Layout || Noop
+export default function MyApp({ Component, pageProps }) {
+  const Layout = Component.Layout || Noop
 
   useEffect(() => {
     document.body.classList?.remove('loading')
